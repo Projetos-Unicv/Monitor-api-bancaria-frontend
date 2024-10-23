f26b9dcc95b2e681e3b1082be6eabf34689a4825
HeytorPires <heytor.cacho29@gmail.com>
2024-10-23 08:44:33 -0300

    :ok_hand: styçe: cores padrão

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "azul-claro": "#0353B3",
+        "azul-medio": "#262D47",
+        "azul-escuro": "#1B213B",
+      },
+    },
   },
   plugins: [],
-}
-
+};
